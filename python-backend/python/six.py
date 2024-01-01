83e640e10eb4c9decf14ad2863b81f8207288371
Huseynov-Samir <samir.huseynov.2395@gmail.com>
2024-01-02 01:42:46 +0400

    six

---
+++ python-backend/python/six.py
@@ -0,0 +1,12 @@
+if __name__ == '__main__':
+    a = int(input().strip())
+    b = int(input().strip())
+
+    # Sum of the two numbers
+    print(a + b)
+
+    # Difference of the two numbers (first - second)
+    print(a - b)
+
+    # Product of the two numbers
+    print(a * b)
